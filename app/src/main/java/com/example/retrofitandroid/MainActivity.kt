5e63092598f2b6418be79867109d935d785c737e
Nitesh Kumar <niteshk@geekyants.com>
2022-12-17 21:35:27 +0530

    Added implementation of Gson library

--- app/src/main/java/com/example/retrofitandroid/MainActivity.kt
+++ app/src/main/java/com/example/retrofitandroid/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.retrofitandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+    var gson = Gson();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var json = convertToJson(TestModel(1,"Test"))
+        convertFromJson(json)
+    }
+
+    private fun convertToJson(testModel: TestModel): String {
+        var json = gson.toJson(testModel)
+        println(json)
+        return json
+    }
+
+    private fun convertFromJson(jsonString: String){
+        var testModel = gson.fromJson(jsonString, TestModel::class.java)
+        println(testModel);
     }
 }
---
+++ app/src/main/java/com/example/retrofitandroid/TestModel.kt
@@ -0,0 +1,7 @@
+package com.example.retrofitandroid
+
+/// we create data class for models
+data class TestModel(
+    val id: Int,
+    val description: String,
+)

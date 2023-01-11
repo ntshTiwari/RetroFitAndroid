23097a54d60e96d6248b71c1b3f32acdb128260d
Nitesh Kumar <niteshk@geekyants.com>
2023-01-12 03:25:05 +0530

    feat: Added pagination and cardview

--- app/src/main/java/com/example/retrofitandroid/MainActivity.kt
+++ app/src/main/java/com/example/retrofitandroid/MainActivity.kt
@@ -13,39 +13,91 @@ class MainActivity : AppCompatActivity() {
     lateinit var newsListView: RecyclerView
     lateinit var newsAdapter: NewsAdapter
 
+    var newsList = mutableListOf<Articles>()
+
+    /// used for pagination
+    var pageNo = 1;
+
+    /// used to restrict calling fetch while fetch is happening
+    var fetching = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         newsListView = findViewById(R.id.newsList)
 
-        newsListView.layoutManager = LinearLayoutManager(this)
+        newsAdapter = NewsAdapter(this@MainActivity, newsList)
+        newsListView.adapter = newsAdapter
+
+        var layoutManager = LinearLayoutManager(this)
+        newsListView.layoutManager = layoutManager
 
         getNews()
+
+        newsListView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                /// we know that total newslist is 38, so we hard coded it here
+                /// we check if we are at the third last element, we fetch rest of the lists
+                if (layoutManager.findLastCompletelyVisibleItemPosition() == newsList.size - 3 && newsList.size != 38) {
+                    if(canFetch()){
+                        Log.e("Paging:", "called")
+                        getNews()
+                    }
+                }
+            }
+        })
+
+
     }
 
     fun getNews() {
+        /// stop any new calls to fetch data
+        blockFetchingData()
+
         val newsInterface = NewsService.newsInterface
 
-        newsInterface.getNews("in", 1).enqueue(
+        newsInterface.getNews("in", pageNo++).enqueue(
             object : Callback<News>{
                 override fun onResponse(call: Call<News>, response: Response<News>) {
+                    /// start accepting new calls to fetch data
+                    unBlockFetchingData()
+
                     val news = response.body()
                     if(news != null){
-                        newsAdapter = NewsAdapter(this@MainActivity, news.articles)
+                        newsList.addAll(news.articles)
+                        /// we notify our news adapter that data has changed
+                        newsAdapter.notifyDataSetChanged()
 
-                        newsListView.adapter = newsAdapter
-
-                        news.articles.forEach {
-                            Log.e("SUCCESS", it.title)
-                        }
+                        Log.e("getNews:", pageNo.toString())
                     }
                 }
 
                 override fun onFailure(call: Call<News>, t: Throwable) {
+                    /// start accepting new calls to fetch data
+                    unBlockFetchingData()
+
                     Log.e("Error", t.toString())
                 }
 
             }
         )
+    }
+
+    /// functions for code clarity
+    private fun blockFetchingData() {
+        fetching = true
+    }
+
+    private fun unBlockFetchingData() {
+        fetching = false
+    }
+
+    private fun canFetch(): Boolean {
+        if(fetching) return false
+
+        return true
     }
 }
